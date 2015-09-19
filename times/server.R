12787c6904e2ee0285077465accdf64a53c10da2
Jeremy Finke <jeremy.finke@gmail.com>
2015-09-19 11:24:59 -0500

    logic for calculating time.

--- times/server.R
+++ times/server.R
@@ -1,7 +1,30 @@
 library(shiny)
+library(lubridate)
 
-# Define server logic required to draw a histogram
+# Define server logic required to output table
+
+
+
 shinyServer(function(input, output) {
+  
+  totalsec <- (input$pminutes*60)+(input$pseconds) #grab input
+  
+  # Calculate time for distance
+  fivek<-(totalsec * 3.1069)
+  fivem<-(totalsec * 5)
+  tenk<-(totalsec * 6.2138)
+  tenm<-(totalsec * 10)
+  halfmara<-(totalsec * 13.10938)
+  mara<-(totalsec * 26.21875)
+  
+  # Covert to human readable time
+  fivek<-round(seconds_to_period(fivek))
+  fivem<-round(seconds_to_period(fivem))
+  tenk<-round(seconds_to_period(tenk))
+  tenm<-round(seconds_to_period(tenm))
+  halfmara<-round(seconds_to_period(halfmara))
+  mara<-round(seconds_to_period(mara))
+  
   output$mytable1 <- renderDataTable({
     
     
