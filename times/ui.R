c9f1db27b2932fcaab6e02511620c4b7e54a5545
Jeremy Finke <jeremy.finke@gmail.com>
2015-09-19 13:48:41 -0500

    added numeric input vs. slider input

--- times/ui.R
+++ times/ui.R
@@ -6,21 +6,28 @@ shinyUI(fluidPage(
   
   # Application title
   titlePanel("Calculate Total Race Times"),
+
+    # Sidebar with a slider inputs for the pace
+    sidebarLayout(
+      sidebarPanel(  
+  numericInput("pminutes", label = h3("Minutes"), value = 8),
+  numericInput("pseconds", label = h3("Seconds"), value = 30)
+  ),
   
-  # Sidebar with a slider inputs for the pace
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("pminutes",
-                  "Pace Minutes:",
-                  min = 1,
-                  max = 20,
-                  value = 8),
-      sliderInput("pseconds",
-                  "Pace Seconds:",
-                  min = 0,
-                  max = 59,
-                  value = 30)
-    ),
+#   # Sidebar with a slider inputs for the pace
+#   sidebarLayout(
+#     sidebarPanel(
+#       sliderInput("pminutes",
+#                   "Pace Minutes:",
+#                   min = 1,
+#                   max = 20,
+#                   value = 8),
+#       sliderInput("pseconds",
+#                   "Pace Seconds:",
+#                   min = 0,
+#                   max = 59,
+#                   value = 30)
+#     ),
     
     # Show a plot of the generated distribution
     mainPanel(
