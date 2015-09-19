d6d0b681ccf1f6a70f5b47956a2f97f8db953385
Jeremy Finke <jeremy.finke@gmail.com>
2015-09-19 10:54:25 -0500

    More code.  

--- projectscratch.R
+++ projectscratch.R
@@ -9,13 +9,21 @@ s<-15
 totalsec<- (h*120)+(m*60)+(s)
 
 fivek<-(totalsec * 3.1069)
+fivem<-(totalsec * 5)
 tenk<-(totalsec * 6.2138)
 tenm<-(totalsec * 10)
-halfmara<-(totalsec * 13.1)
-mara<-(totalsec * 26.2)
+halfmara<-(totalsec * 13.10938)
+mara<-(totalsec * 26.21875)
 
-seconds_to_period(fivek)
-seconds_to_period(tenk)
-seconds_to_period(tenm)
-seconds_to_period(halfmara)
-seconds_to_period(mara)
+round(seconds_to_period(fivek))
+round(seconds_to_period(fivem))
+round(seconds_to_period(tenk))
+round(seconds_to_period(tenm))
+round(seconds_to_period(halfmara))
+round(seconds_to_period(mara))
+
+
+half<-seconds_to_period(halfmara)
+round(half)
+
+
