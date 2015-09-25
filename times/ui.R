e25c345fbc493c8749141e9690b424d92882efba
Jeremy Finke <jeremy.finke@gmail.com>
2015-09-25 18:27:51 -0500

    Removed slider inputs.

--- times/ui.R
+++ times/ui.R
@@ -10,26 +10,10 @@ shinyUI(fluidPage(
     # Sidebar with a slider inputs for the pace
     sidebarLayout(
       sidebarPanel(  
-  numericInput("pminutes", label = h3("Minutes"), value = 8),
-  numericInput("pseconds", label = h3("Seconds"), value = 30)
+  numericInput("pminutes", label = h4("Minutes"), value = 8),
+  numericInput("pseconds", label = h4("Seconds"), value = 30)
   ),
   
-#   # Sidebar with a slider inputs for the pace
-#   sidebarLayout(
-#     sidebarPanel(
-#       sliderInput("pminutes",
-#                   "Pace Minutes:",
-#                   min = 1,
-#                   max = 20,
-#                   value = 8),
-#       sliderInput("pseconds",
-#                   "Pace Seconds:",
-#                   min = 0,
-#                   max = 59,
-#                   value = 30)
-#     ),
-    
-    # Show a plot of the generated distribution
     mainPanel(
       tableOutput('mytable1')
     )
